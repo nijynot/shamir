aed51aeea199c113248fc2b0a1c1c6ea5d0dd30e
nijynot <nijynot@gmail.com>
2018-07-19 00:36:39 +0200

    Add README.md

---
+++ README.md
@@ -0,0 +1,55 @@
+# shamir
+
+## Usage
+
+Example:
+```js
+
+const shamir = require('./index.js');
+
+// Exmaple Bitcoin private key from https://en.bitcoin.it/wiki/Private_key
+const example = '0xe9873d79c6d87dc0fb6a5778633389f4453213303da61f20bd67fc233aa33262';
+const shares = shamir.split(example, 6, 3, shamir.mersenne);
+// => [{ x: 1, y: 0x... }, { x: 2, y: 0x... }, ... ,{ x: 6, y: 0x... }]
+
+const secret = shamir.combine([shares[0], shares[1], shares[2]], shamir.mersenne).toHex();
+// => 0xe9873d79c6d87dc0fb6a5778633389f4453213303da61f20bd67fc233aa33262
+```
+
+## API
+
+### `shamir.split(secret, n, k, prime)`
+##### `secret`
+
+Type: `String`
+
+##### `n`
+Type: `Number`
+
+The amount of shares you want to generate.
+
+##### `k`
+Type: `Number`
+
+The minumum amount of shares required to reconstruct the secret.
+
+##### `prime`
+Type: `String` or `Number`
+
+A prime number.
+
+### `shamir.combine(shares, prime)`
+##### `shares`
+Type: `Array`
+
+Array of your shares, where each share is an `Object` with corresponding `x` and `y` values generated from ``shamir.split.
+
+##### `prime`
+Type: `String` or `Number`
+
+A prime number.
+
+### `shamir.mersenne`
+Type: `Decimal` (Decimal from decimal.js)
+
+Returns the mersenne prime, 2**512 - 1.
--- index.js
+++ index.js
@@ -6,9 +6,9 @@ Decimal.set({ modulo: Decimal.ROUND_FLOOR });
 Decimal.set({ crypto: true });
 Decimal.set({ precision: 1000000 });
 Decimal.set({ toExpPos: 90000000000000 });
-// const mersenne = Decimal(2**127 - 1);
+
+const exampleKey = '0xe9873d79c6d87dc0fb6a5778633389f4453213303da61f20bd67fc233aa33262';
 const mersenne = Decimal(2**512 - 1);
-const sample_key = Decimal('0xe9873d79c6d87dc0fb6a5778633389f4453213303da61f20bd67fc233aa33262');
 
 function random(lower, upper) {
   if (lower > upper) {
@@ -16,15 +16,9 @@ function random(lower, upper) {
     lower = upper;
     upper = temp;
   }
+
   return lower.add(Decimal.random().times(upper.sub(lower + 1)).floor());
 }
-
-const mySecret = Decimal(1234);
-const myShares = [
-  { x: Decimal(3), y: Decimal(2) },
-  { x: Decimal(4), y: Decimal(1) },
-  { x: Decimal(5), y: Decimal(2) },
-];
 
 function q(x, { a }) {
   let value = a[0];
@@ -35,40 +29,34 @@ function q(x, { a }) {
   return value;
 }
 
-function split(S, n, k, prime) {
+function split(secret, n, k, prime) {
+  const S = Decimal(secret);
+  const p = Decimal(prime);
   let a = [S];
   let D = [];
 
   for (let i = 1; i < k; i++) {
-    a.push(random(Decimal(0), Decimal(prime).sub(1)));
+    a.push(random(Decimal(0), p.sub(1)));
   }
 
   for (let i = 0; i < n; i++) {
     let x = Decimal(i + 1);
     D.push({
       x,
-      y: q(x, { a }).mod(prime),
+      y: q(x, { a }).mod(p),
     });
   }
 
-  let string = '';
-  let string2 = '';
-
-  for (let i = 0; i < a.length; i++) {
-    string = `${string}a_${i}: ${a[i].toString()}\n`;
-  }
-  for (let i = 0; i < D.length; i++) {
-    string2 = `${string2}D_${i}: (${D[i].x.toNumber()}, ${D[i].y.toHex()})\n`;
-  }
-  console.log(a[0].toHex());
-  console.log(string);
-  console.log(string2);
-
-  return D;
+  return D.map((share) => ({
+    x: share.x.toString(),
+    y: share.y.toHex(),
+  }));
 }
 
 function lagrangeBasis(data, j) {
-  // Lagrange basis evaluated at 0; L(0).
+  // Lagrange basis evaluated at 0, i.e. L(0).
+  // You don't need to interpolate the whole polynomial to get the secret, you
+  // only need the constant term.
   let denominator = Decimal(1);
   let numerator = Decimal(1);
   for (let i = 0; i < data.length; i++) {
@@ -100,24 +88,19 @@ function lagrangeInterpolate(data, p) {
   return S.mod(p);
 }
 
-function combine(shares, p) {
-  return lagrangeInterpolate(shares, p);
+function combine(shares, prime) {
+  const p = Decimal(prime);
+
+  // Wrap with Decimal on the input shares
+  const decimalShares = shares.map((share) => ({
+    x: Decimal(share.x),
+    y: Decimal(share.y),
+  }));
+
+  return lagrangeInterpolate(decimalShares, p);
 }
 
-const sample_shares = [
-  {
-    x: Decimal(1),
-    y: Decimal('0xdad7a51746faca3cab57875fad5d1c77347bbc037d59d7dbb865c6ad4470a4f3c140175e5df59bcaadc26fe013fb197b70216f0cf6648403e6cf54cbf78efcd7'),
-  },
-  {
-    x: Decimal(2),
-    y: Decimal('0xa7814787e649bffb9571a9cdd115057d3025cd1b3dd252400be3f251c977c034284d26abe439c6d6aaf42f1deec14f6c8df8d9f58a551841f8f601326a162794'),
-  },
-  {
-    x: Decimal(3),
-    y: Decimal('0x65fce751ddece0b3bf521a0d6db56a6f49456d46ebf4b77f33070a51c6af15563883b2fd08b2123486713c80b09a9fc79eb853e9f977dbdaf3dc01569238b299'),
-  },
-];
-
-// split(sample_key, 6, 3, Decimal(2**512 - 1));
-console.log(combine(sample_shares, Decimal(2**512 - 1)).toHex());
+exports.exampleKey = exampleKey;
+exports.mersenne = mersenne;
+exports.split = split;
+exports.combine = combine;
